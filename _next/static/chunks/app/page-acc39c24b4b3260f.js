bfdad1ec7136ca971ea8ae55f1b9a6ba03da07ae
sam961124 <sam961124@gmail.com>
2023-09-09 07:37:00 +0000

    Deploying to public from @ sam961124/frontend-mentor-qrcode@8a032715a747d74962328dadd822df6109e44b5a 🚀

--- _next/static/chunks/app/page-acc39c24b4b3260f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3678:function(e,n,s){Promise.resolve().then(s.t.bind(s,3222,23)),Promise.resolve().then(s.t.bind(s,8974,23)),Promise.resolve().then(s.t.bind(s,9703,23))}},function(e){e.O(0,[698,971,596,744],function(){return e(e.s=3678)}),_N_E=e.O()}]);
