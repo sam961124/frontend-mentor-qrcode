0085401223f4a7e6f7db0fb21ba3d2721c2b1e86
Sean Lee <sam961124@gmail.com>
2023-09-09 14:52:14 +0800

    Fix imgix

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     basePath: basePath,
     images: {
       loader: 'imgix',
-      path: 'shangenlee-878868498.imgix.net',
+      path: 'https://shangenlee-878868498.imgix.net',
     },
 };
 
